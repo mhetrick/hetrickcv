0de9fb5850fb9991007f926b61a10e1d5cfc46a9
Michael Hetrick <michael.s.hetrick@gmail.com>
2017-11-28 23:09:00 -0600

    Expand Gate Combiner

--- src/LogicCombine.cpp
+++ src/LogicCombine.cpp
@@ -14,6 +14,8 @@ struct LogicCombine : Module
         IN4_INPUT,
         IN5_INPUT,
         IN6_INPUT,
+        IN7_INPUT,
+        IN8_INPUT,
 		NUM_INPUTS
 	};
 	enum OutputIds 
@@ -32,11 +34,11 @@ struct LogicCombine : Module
         NUM_LIGHTS
 	};
 
-    bool ins[6] = {};
-    bool trigs[6] = {};
+    bool ins[NUM_INPUTS] = {};
+    bool trigs[NUM_INPUTS] = {};
     float outs[3] = {};
     float trigLight;
-    SchmittTrigger inTrigs[6];
+    SchmittTrigger inTrigs[NUM_INPUTS];
     bool orState = false;
     bool trigState = false;
     const float lightLambda = 0.075;
@@ -59,23 +61,17 @@ struct LogicCombine : Module
 
 void LogicCombine::step() 
 {
-    ins[0] = (inputs[IN1_INPUT].value >= 1.0f);
-    ins[1] = (inputs[IN2_INPUT].value >= 1.0f);
-    ins[2] = (inputs[IN3_INPUT].value >= 1.0f);
-    ins[3] = (inputs[IN4_INPUT].value >= 1.0f);
-    ins[4] = (inputs[IN5_INPUT].value >= 1.0f);
-    ins[5] = (inputs[IN6_INPUT].value >= 1.0f);
+    orState = false;
+    trigState = false;
 
-    orState = (ins[0] || ins[1] || ins[2] || ins[3] || ins[4] || ins[5]);
+    for(int i = 0; i < NUM_INPUTS; i++)
+    {
+        ins[i] = (inputs[IN1_INPUT + i].value >= 1.0f);
+        trigs[i] = inTrigs[i].process(inputs[IN1_INPUT + i].value);
 
-    trigs[0] = inTrigs[0].process(inputs[IN1_INPUT].value);
-    trigs[1] = inTrigs[1].process(inputs[IN2_INPUT].value);
-    trigs[2] = inTrigs[2].process(inputs[IN3_INPUT].value);
-    trigs[3] = inTrigs[3].process(inputs[IN4_INPUT].value);
-    trigs[4] = inTrigs[4].process(inputs[IN5_INPUT].value);
-    trigs[5] = inTrigs[5].process(inputs[IN6_INPUT].value);
-
-    trigState = (trigs[0] || trigs[1] || trigs[2] || trigs[3] || trigs[4] || trigs[5]);
+        orState = orState || ins[i];
+        trigState = trigState || trigs[i];
+    }
 
     outs[0] = orState ? 5.0f : 0.0f;
     outs[1] = 5.0f - outs[0];
@@ -105,7 +101,7 @@ LogicCombineWidget::LogicCombineWidget()
 {
 	auto *module = new LogicCombine();
 	setModule(module);
-	box.size = Vec(6 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT);
+	box.size = Vec(8 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT);
 
 	{
 		auto *panel = new SVGPanel();
@@ -122,20 +118,22 @@ LogicCombineWidget::LogicCombineWidget()
     //////PARAMS//////
 
     //////INPUTS//////
-    addInput(createInput<PJ301MPort>(Vec(10, 60), module, LogicCombine::IN1_INPUT));
-    addInput(createInput<PJ301MPort>(Vec(10, 105), module, LogicCombine::IN2_INPUT));
-    addInput(createInput<PJ301MPort>(Vec(10, 150), module, LogicCombine::IN3_INPUT));
-    addInput(createInput<PJ301MPort>(Vec(10, 195), module, LogicCombine::IN4_INPUT));
-    addInput(createInput<PJ301MPort>(Vec(10, 240), module, LogicCombine::IN5_INPUT));
-    addInput(createInput<PJ301MPort>(Vec(10, 285), module, LogicCombine::IN6_INPUT));
+    const int inSpacing = 40;
+    const int outPos = 67;
+    const int lightPos = outPos + 29;
+
+    for(int i = 0; i < LogicCombine::NUM_INPUTS; i++)
+    {
+        addInput(createInput<PJ301MPort>(Vec(10, 50 + (i*inSpacing)), module, LogicCombine::IN1_INPUT + i));
+    }
 
     //////OUTPUTS//////
-    addOutput(createOutput<PJ301MPort>(Vec(45, 150), module, LogicCombine::OR_OUTPUT));
-    addOutput(createOutput<PJ301MPort>(Vec(45, 195), module, LogicCombine::NOR_OUTPUT));
-    addOutput(createOutput<PJ301MPort>(Vec(45, 240), module, LogicCombine::TRIG_OUTPUT));
+    addOutput(createOutput<PJ301MPort>(Vec(outPos, 150), module, LogicCombine::OR_OUTPUT));
+    addOutput(createOutput<PJ301MPort>(Vec(outPos, 195), module, LogicCombine::NOR_OUTPUT));
+    addOutput(createOutput<PJ301MPort>(Vec(outPos, 240), module, LogicCombine::TRIG_OUTPUT));
 
     //////BLINKENLIGHTS//////
-    addChild(createLight<SmallLight<RedLight>>(Vec(74, 158), module, LogicCombine::OR_LIGHT));
-    addChild(createLight<SmallLight<RedLight>>(Vec(74, 203), module, LogicCombine::NOR_LIGHT));
-    addChild(createLight<SmallLight<RedLight>>(Vec(74, 248), module, LogicCombine::TRIG_LIGHT));
+    addChild(createLight<SmallLight<RedLight>>(Vec(lightPos, 158), module, LogicCombine::OR_LIGHT));
+    addChild(createLight<SmallLight<RedLight>>(Vec(lightPos, 203), module, LogicCombine::NOR_LIGHT));
+    addChild(createLight<SmallLight<RedLight>>(Vec(lightPos, 248), module, LogicCombine::TRIG_LIGHT));
 }
