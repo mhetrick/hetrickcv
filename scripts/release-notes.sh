8e7294976eb653e68feaa4d1520673d1dfd19e37
Michael Hetrick <michael.s.hetrick@gmail.com>
2019-06-26 15:25:51 -0500

    Fix script permissions

--- scripts/release-notes.sh
+++ scripts/release-notes.sh
@@ -2,14 +2,18 @@
 
 cat <<- EOH
 # Automatically generated HetrickCV release
-(Thanks to baconpaul for the guide on setting this up!)
+
+Thanks to baconpaul for the guide on setting this up!
 https://community.vcvrack.com/t/azure-pipelines-for-github-allows-build-of-plugin/2651
+
 This release is automatically generated every time I push HetrickCV
 to master. As such it is the latest version of the code and may be
 unstable, unusable, unsuitable for human consumption, and so on.
 The stable release is always available in the Rack Plugin Manager.
+
 These assets were built against
 https://vcvrack.com/downloads/Rack-SDK-1.0.0.zip
+
 The build date and most recent commits are:
 EOH
 date
