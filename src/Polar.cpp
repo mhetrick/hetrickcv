ae4f516c85a96c6b77c7d331a5d7be412058ebdf
Michael Hetrick <michael.s.hetrick@gmail.com>
2021-10-31 23:11:53 -0500

    Make XY to Polar polyphonic

--- src/Polar.cpp
+++ src/Polar.cpp
@@ -37,6 +37,11 @@ struct XYToPolar : HCVModule
 
 	void process(const ProcessArgs &args) override;
 
+    simd::float_4   insX[4] = {0.0f, 0.0f, 0.0f, 0.0f}, insY[4] = {0.0f, 0.0f, 0.0f, 0.0f},
+                    insR[4] = {0.0f, 0.0f, 0.0f, 0.0f}, insTheta[4] = {0.0f, 0.0f, 0.0f, 0.0f},
+                    outsX[4] = {0.0f, 0.0f, 0.0f, 0.0f}, outsY[4] = {0.0f, 0.0f, 0.0f, 0.0f},
+                    outsR[4] = {0.0f, 0.0f, 0.0f, 0.0f}, outsTheta[4] = {0.0f, 0.0f, 0.0f, 0.0f};
+
 	// For more advanced Module features, read Rack's engine.hpp header file
 	// - dataToJson, dataFromJson: serialization of internal data
 	// - onSampleRateChange: event triggered by a change of sample rate
@@ -46,26 +51,40 @@ struct XYToPolar : HCVModule
 
 void XYToPolar::process(const ProcessArgs &args)
 {
-    auto x = inputs[INX_INPUT].getVoltage();
-    auto y = inputs[INY_INPUT].getVoltage();
+    int channels = getMaxInputPolyphony();
+    outputs[OUTX_OUTPUT].setChannels(channels);
+    outputs[OUTY_OUTPUT].setChannels(channels);
+    outputs[OUTR_OUTPUT].setChannels(channels);
+    outputs[OUTTHETA_OUTPUT].setChannels(channels);
 
-    double r = sqrt(x*x + y*y);
-    double theta;
-    
-    if(x == 0.0 && y == 0.0) theta = 0.0;
-    else theta = atan2(y, x);
+    bool rConnected = inputs[INR_INPUT].isConnected();
+    bool thetaConnected = inputs[INTHETA_INPUT].isConnected();
 
-    outputs[OUTR_OUTPUT].setVoltage(r);
-    outputs[OUTTHETA_OUTPUT].setVoltage(theta);
+	for (int c = 0; c < channels; c += 4) 
+	{
+        const int vectorIndex = c / 4;
+		insX[vectorIndex] = simd::float_4::load(inputs[INX_INPUT].getVoltages(c));
+        insY[vectorIndex] = simd::float_4::load(inputs[INY_INPUT].getVoltages(c));
 
-    if(inputs[INR_INPUT].isConnected()) r = inputs[INR_INPUT].getVoltage();
-    if(inputs[INTHETA_INPUT].isConnected()) theta = inputs[INTHETA_INPUT].getVoltage();
+        outsR[vectorIndex] = sqrt(insX[vectorIndex] * insX[vectorIndex] + insY[vectorIndex] * insY[vectorIndex]);
+        outsTheta[vectorIndex] = atan2(insY[vectorIndex], insX[vectorIndex]);
 
-    x = r * cos(theta);
-    y = r * sin(theta);
+        for (int i = 0; i < 4; i++)
+        {
+            if(!isnormal(outsTheta[vectorIndex][i])) outsTheta[vectorIndex][i] = 0.0f;
+        }
 
-    outputs[OUTX_OUTPUT].setVoltage(x);
-    outputs[OUTY_OUTPUT].setVoltage(y);
+        insR[vectorIndex] = rConnected ? simd::float_4::load(inputs[INR_INPUT].getVoltages(c)) : outsR[vectorIndex];
+        insTheta[vectorIndex] = thetaConnected ? simd::float_4::load(inputs[INTHETA_INPUT].getVoltages(c)) : outsTheta[vectorIndex];
+
+        outsX[vectorIndex] = insR[vectorIndex] * cos(insTheta[vectorIndex]);
+        outsY[vectorIndex] = insR[vectorIndex] * sin(insTheta[vectorIndex]);
+
+        outsR[vectorIndex].store(outputs[OUTR_OUTPUT].getVoltages(c));
+        outsTheta[vectorIndex].store(outputs[OUTTHETA_OUTPUT].getVoltages(c));
+        outsX[vectorIndex].store(outputs[OUTX_OUTPUT].getVoltages(c));
+        outsY[vectorIndex].store(outputs[OUTY_OUTPUT].getVoltages(c));
+	}
 }
 
 struct XYToPolarWidget : HCVModuleWidget { XYToPolarWidget(XYToPolar *module); };
